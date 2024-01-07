753b9caa7f5fbe628abc1b28964751351c085ed7
Asad <muhammadasad98980@gmail.com>
2024-01-07 17:10:54 +0500

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import Navbar from './components/Navbar';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Home from './components/Home';
+import About from './components/About';
 function App() {
   return (
     <>
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+
+        </Switch>
+      </Router>
     </>
+
   );
 }
 
